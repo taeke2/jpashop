b38875de91d6a0f8d91c60d2e547703e0d277086
taeke2 <taeke2@naver.com>
2023-07-13 15:09:09 +0900

    lombok Getter&Setter 적용해보기

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 
 	public static void main(String[] args) {
+		Hello hello = new Hello();
+		hello.setData("data");
+		System.out.println("data = " + hello.getData());
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
