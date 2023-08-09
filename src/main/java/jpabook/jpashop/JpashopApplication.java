a38ef5afad9531774a6ad711bfe5b6ae57f9e1d5
taeke2 <taeke2@naver.com>
2023-08-09 21:41:43 +0900

    엔티티 클래스 개발

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -7,9 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 
 	public static void main(String[] args) {
-		Hello hello = new Hello();
-		hello.setData("data");
-		System.out.println("data = " + hello.getData());
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
